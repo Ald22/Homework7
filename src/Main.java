c2d8e68a09a47d0d0d26b7a0af88d1534091bf00
Алдар <aldar.nanzitovich@gmail.com>
2022-11-04 18:14:25 +0800

    Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Домашнее задание 1
+        System.out.println("Домашнее задание 1");
+        // Задание 1
+        System.out.println("Задание 1");
+    }
+}
